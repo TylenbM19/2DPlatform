3be501a943207e49ff4e78b2c52a756d8523a12c
Igor <e-e-dsf-dsf@yandex.ru>
2022-07-04 20:54:19 +0300

    24

--- Assets/Skripts/PlayerControler.cs
+++ Assets/Skripts/PlayerControler.cs
@@ -6,8 +6,9 @@ public class PlayerControler : Controler
 {
     private void Update()
     {
-        InvokeEventMove(new Vector2(Input.GetAxis("Horizontal"), 0f));
+        InvokeEventControlerMove(new Vector2(Input.GetAxis("Horizontal"), 0f));
+
         if (Input.GetKeyDown(KeyCode.W))
-            InvokeEventJump();
+            InvokeEventControlerJump();
     }
 }
